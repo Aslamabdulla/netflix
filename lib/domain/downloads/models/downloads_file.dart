5e6e04a3c478479bcf7e7a39a9533a81d1c9f27e
Aslamabdulla <themanwithgunpower@gmail.com>
2022-09-22 07:42:21 +0530

    commit fixed bug

--- lib/domain/downloads/models/downloads_file.dart
+++ lib/domain/downloads/models/downloads_file.dart
@@ -1,11 +1,14 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 part 'downloads_file.freezed.dart';
 part 'downloads_file.g.dart';
 
 @freezed
 class Downloads with _$Downloads {
-  const factory Downloads() = _Downloads;
-@JsonKey(name: "poster_path") required String? posterPath;
+  const factory Downloads({
+    @JsonKey(name: "poster_path") required String? posterPath,
+  }) = _Downloads;
+
   factory Downloads.fromJson(Map<String, dynamic> json) =>
       _$DownloadsFromJson(json);
 }
